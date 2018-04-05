5229cd87892b5614c20b00f2e3ea46da72f4aa0f
Jeff Hultman <hultmanjeff@gmail.com>
2018-04-05 13:13:17 -0700

    .

--- readInto2DArray.c
+++ readInto2DArray.c
@@ -1,17 +1,15 @@
 #include<stdio.h>
 int main(){
 
-   int disp[9][9];
-
+   int puzzle[9][9];
    int i, j;
-   for(i=0; i<9; i++) {
-      for(j=0;j<9;j++) {
-         // printf("Enter value for disp[%d][%d]:", i, j);
+   for(i=0; i<9; i++)
+   {
+      for(j=0;j<9;j++)
+      {
          scanf("%d", &disp[i][j]);
       }
    }
-
-   printf("Two Dimensional array elements:\n");
    for(i=0; i<9; i++)
    {
       for(j=0;j<9;j++)
