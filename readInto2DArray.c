7e7c56a76b53c0d1118f6124d7c11d6f097bcbfa
Jeff Hultman <hultmanjeff@gmail.com>
2018-04-05 14:04:15 -0700

    Takes input from user and creates 2D array

--- readInto2DArray.c
+++ readInto2DArray.c
@@ -1,25 +1,27 @@
 #include<stdio.h>
-int main(){
+int main()
+{
+  int puzzle[9][9];
+  int i, j;
 
-   int puzzle[9][9];
-   int i, j;
-   for(i=0; i<9; i++)
-   {
-      for(j=0;j<9;j++)
+
+  for(i=0; i<9; i++)
+  {
+    for(j=0;j<9;j++)
+    {
+      scanf("%d", &puzzle[i][j]);
+    }
+  }
+  for(i=0; i<9; i++)
+  {
+    for(j=0;j<9;j++)
+    {
+      printf("%d ", puzzle[i][j]);
+      if(j==8)
       {
-         scanf("%d", &puzzle[i][j]);
+        printf("\n");
       }
-   }
-   for(i=0; i<9; i++)
-   {
-      for(j=0;j<9;j++)
-      {
-         printf("%d ", puzzle[i][j]);
-         if(j==8)
-         {
-            printf("\n");
-         }
-      }
-   }
-   return 0;
+    }
+  }
+  return 0;
 }
